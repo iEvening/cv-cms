9109b5c3319aaf5e01f8d257837da3f63579ed6a
iEvening <122746369+iEvening@users.noreply.github.com>
2023-02-09 02:11:52 +0100

    Update apiHandlers.ts
    Initialize the first handler

--- lib/handlers/apiHandlers.ts
+++ lib/handlers/apiHandlers.ts
@@ -1,3 +1,5 @@
-export function checkSecure(){
+import load from "@/lib/load";
 
+export async function getSecureMenuContent() {
+    return await load({url: "/api/menu", options: {method: "GET"}});
 }
